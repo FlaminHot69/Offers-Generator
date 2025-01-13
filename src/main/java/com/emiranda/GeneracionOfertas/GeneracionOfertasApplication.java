389786fef9668677f4848a51ee9487eb399cc2da
FlaminHot69 <emilianomiranda7@hotmail.com>
2025-01-12 21:01:49 -0600

    init commit

---
+++ src/main/java/com/emiranda/GeneracionOfertas/GeneracionOfertasApplication.java
@@ -0,0 +1,13 @@
+package com.emiranda.GeneracionOfertas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneracionOfertasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneracionOfertasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emiranda/GeneracionOfertas/GeneracionOfertasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emiranda.GeneracionOfertas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneracionOfertasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
