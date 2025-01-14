b3703ee0df93a8bac27cf07eaff32d50e2caae5e
FlaminHot69 <emilianomiranda7@hotmail.com>
2025-01-14 00:33:51 -0600

    Configuración de la base de datos PostgreSQL y el archivo de configuración para el logger.

--- src/main/java/com/emiranda/GeneracionOfertas/GeneracionOfertasApplication.java
+++ src/main/java/com/emiranda/GeneracionOfertas/GeneracionOfertasApplication.java
@@ -1,13 +1,18 @@
 package com.emiranda.GeneracionOfertas;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GeneracionOfertasApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(GeneracionOfertasApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(GeneracionOfertasApplication.class, args);
+		logger.info("Application started successfully");
 	}
 
 }
